3e8379d3f0b00e503b8bd5007b669168e502570a
Kaushal Krishna <132030841+kaushal-krishna@users.noreply.github.com>
2024-07-28 07:27:45 +0530

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import TodayPicks from "../components/home/TodayPicks";
 import Post from "../components/home/Post";
+import HomeNavbar from "../layout/items/HomeNavbar.js";
 export default function Home() {
   return (
     <>
       <section id="home">
+      <HomeNavbar />
         <div className="home-container">
           <TodayPicks />
           <Post />
