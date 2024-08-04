48bb02fdd445c0b06e164c9f5e17d755402e20a2
Kaushal Krishna <kaushalkrishna011@gmail.com>
2024-08-04 11:20:24 +0530

    `date`

--- src/components/music/TrackDeck.js
+++ src/components/music/TrackDeck.js
@@ -19,70 +19,70 @@ const TrackDeck = () => {
     lyricsWrapperRef.current.innerHTML = currentTrackLyrics;
   };
 
-  // useEffect(() => {
-  //   if (currentTrack.hasLyrics) {
-  //     getTrackLyrics();
-  //   } else {
-  //     lyricsWrapperRef.current.innerHTML = "Couldn't load lyrics for the song.";
-  //   }
-  // }, [currentTrack.id]);
+  useEffect(() => {
+    if (currentTrack.hasLyrics) {
+      getTrackLyrics();
+    } else {
+      lyricsWrapperRef.current.innerHTML = "Couldn't load lyrics for the song.";
+    }
+  }, [currentTrack.id]);
 
   return (
     <div className="track-deck">
       <div className="track-deck--cover">
-        <img src="https://c.saavncdn.com/252/Laapataa-Ladies-Hindi-2024-20240213151004-500x500.jpg" />
+        <img src={currentTrack.image} />
       </div>
       <div className="track-deck--details">
         <label className="track-deck--details-name">
-          {currentTrack.name}Sajni Re
+          {currentTrack.name}
         </label>
         <label className="track-deck--details-artists">
-          {currentTrack.artists}Ram Sampath, Arijit Singh, Prashant Pandey
+          {currentTrack.artists}
         </label>
       </div>
+      <div
+        // ref={progressBarRef}
+        // onClick={handleProgressBarClick}
+        // onMouseDown={handleProgressBarMouseDown}
+        // onMouseMove={handleProgressBarDrag}
+        // onMouseUp={handleProgressBarMouseUp}
+        // onTouchStart={handleTouchStart}
+        // onTouchMove={handleTouchMove}
+        // onTouchEnd={handleTouchEnd}
+        style={{
+          width: "90%",
+          height: "4px",
+          backgroundColor: "#4e4e4e",
+          borderRadius: "5px",
+          margin: "0 0",
+          position: "relative",
+          cursor: "pointer",
+        }}
+      >
         <div
-          // ref={progressBarRef}
-          // onClick={handleProgressBarClick}
-          // onMouseDown={handleProgressBarMouseDown}
-          // onMouseMove={handleProgressBarDrag}
-          // onMouseUp={handleProgressBarMouseUp}
-          // onTouchStart={handleTouchStart}
-          // onTouchMove={handleTouchMove}
-          // onTouchEnd={handleTouchEnd}
           style={{
-            width: "90%",
             height: "4px",
-            backgroundColor: "#4e4e4e",
-            borderRadius: "5px",
-            margin: "0 0",
-            position: "relative",
+            borderRadius: "4px",
+            backgroundColor: "#fff",
+            width: `40%`,
+          }}
+        ></div>
+        <div
+          style={{
+            position: "absolute",
+            top: "-3px",
+            left: `40%`,
+            transform: "translateX(-50%)",
+            width: "10px",
+            height: "10px",
+            borderRadius: "50%",
+            backgroundColor: "#fff",
+            boxShadow: "0 0 5px rgba(0, 0, 0, 0.3)",
             cursor: "pointer",
           }}
-        >
-          <div
-            style={{
-              height: "4px",
-              borderRadius: "4px",
-              backgroundColor: "#fff",
-              width: `40%`,
-            }}
-          ></div>
-          <div
-            style={{
-              position: "absolute",
-              top: "-3px",
-              left: `40%`,
-              transform: "translateX(-50%)",
-              width: "10px",
-              height: "10px",
-              borderRadius: "50%",
-              backgroundColor: "#fff",
-              boxShadow: "0 0 5px rgba(0, 0, 0, 0.3)",
-              cursor: "pointer",
-            }}
-          ></div>
-        </div>
-              <div className="track-deck--controls">
+        ></div>
+      </div>
+      <div className="track-deck--controls">
         <span className="track-deck--controls-item">
           <svg role="img" aria-hidden="true" viewBox="0 0 24 24">
             <path d="m7.05 3.606 13.49 7.788a.7.7 0 0 1 0 1.212L7.05 20.394A.7.7 0 0 1 6 19.788V4.212a.7.7 0 0 1 1.05-.606z"></path>
@@ -96,56 +96,10 @@ const TrackDeck = () => {
       </div>
       <div className="track-deck--lyrics">
         <p>Lyrics</p>
-        <div className="track-deck--lyrics-wrapper" ref={lyricsWrapperRef}>
-          o, sajani re
-          <br />
-          kaise katen din-raat?
-          <br />
-          kaise ho tujh se baath?
-          <br />
-          teri yaad sataave re
-          <br />
-          <br />
-          o, sajani re
-          <br />
-          kaise katen din-raat?
-          <br />
-          kaise milee tera saath?
-          <br />
-          teri yaad, teri yaad sataave re
-          <br />
-          <br />
-          kaise ghanere badaraa ghiren
-          <br />
-          teri kamee kee baarish liye?
-          <br />
-          sailaab jo mere seene mein hai
-          <br />
-          koyi bataae, ye kaise thame
-          <br />
-          tere binaa ab kaise jien?
-          <br />
-          <br />
-          o, sajani re
-          <br />
-          kaise katen din-raat?
-          <br />
-          kaise ho tujh se baath?
-          <br />
-          teri yaad sataave re
-          <br />
-          <br />
-          o, sajani re
-          <br />
-          kaise katen din-raat?
-          <br />
-          kaise ho tujh se baath?
-          <br />
-          teri yaad, teri yaad sataave re
-          <br />
-          <br />
-          o, sajani re
-        </div>
+        <div
+          className="track-deck--lyrics-wrapper"
+          ref={lyricsWrapperRef}
+        ></div>
       </div>
     </div>
   );
